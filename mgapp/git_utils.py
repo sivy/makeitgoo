c0613c9faebebaf3da04ee7528ace361c560de5e
Steve Ivy <steve@wallrazer.com>
2011-04-05 17:40:39 -0700

    add wrappers for useful git commands

--- mgapp/git_utils.py
+++ mgapp/git_utils.py
@@ -1,10 +1,49 @@
-import subprocess, shlex
+from __future__ import with_statement
+from fabric.api import *
 
-def _run(cmd):
-    cmd = shlex.split(cmd)
-    return subprocess.Popen(cmds, stdout=subprocess.PIPE).communicate()[0]
+def _run(cmd, wd=None, local=True):
+    if not wd:
+        wd = "."
+    with cd(wd):
+        if local:
+            return local(cmd, capture=True)
+        else:
+            return run(cwd, capture=True)
 
-def current_commitish():
+def current_commit(wd=None, local=True):
     cmd = 'git log -n1 --pretty="%H"'
-    return _run(cmd)
+    return _run(cmd, wd, local=local)
 
+# ---
+
+def checkout(wd=None, local=True):
+    cmd = 'git checkout'
+    return _run(cmd, wd=wd, local=local)
+    
+def branch(wd=None, local=True):
+    cmd = 'git rev-parse --symbolic-full-name --abbrev-ref HEAD'
+    return _run(cmd, wd=wd, local=local)
+
+def set_remote(wd=None, local=True):
+    cmd = 'git remote set-url origin'
+    return _run(cmd, wd=wd, local=local)
+
+def status(wd=None, local=True):
+    cmd = 'git status'
+    return _run(cmd, wd=wd, local=local)
+
+def merge_base(wd=None, local=True):
+    cmd = 'git merge-base'
+    return _run(cmd, wd=wd, local=local)
+
+def upstream(wd=None, local=True):
+    cmd = 'git rev-parse @{u}'
+    return _run(cmd, wd=wd, local=local)
+
+def head(wd=None, local=True):
+    cmd = 'git rev-parse HEAD'
+    return _run(cmd, wd=wd, local=local)
+
+def remote_url(wd=None, local=True):
+    cmd = 'git config --get remote.origin.url'
+    return _run(cmd, wd=wd, local=local)
