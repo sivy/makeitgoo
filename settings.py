df0dd5789a61f48e7664772ca93ab643cd94b018
Steve Ivy <steve@wallrazer.com>
2011-04-08 12:24:02 -0700

    remove db info (moved to local), add south in apps again

--- settings.py
+++ settings.py
@@ -9,17 +9,6 @@ ADMINS = (
 
 MANAGERS = ADMINS
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3', # Add 'postgresql_psycopg2', 'postgresql', 'mysql', 'sqlite3' or 'oracle'.
-        'NAME': '/var/makeitgoo/makeitgoo/mig.sqlite3',                      # Or path to database file if using sqlite3.
-        'USER': '',                      # Not used with sqlite3.
-        'PASSWORD': '',                  # Not used with sqlite3.
-        'HOST': '',                      # Set to empty string for localhost. Not used with sqlite3.
-        'PORT': '',                      # Set to empty string for default. Not used with sqlite3.
-    }
-}
-
 # Local time zone for this installation. Choices can be found here:
 # http://en.wikipedia.org/wiki/List_of_tz_zones_by_name
 # although not all choices may be available on all operating systems.
@@ -97,7 +86,7 @@ INSTALLED_APPS = (
     # 'django.contrib.admindocs',
     'nexus',
     'gargoyle',
-    # 'south'
+    'south'
 )
 
 from local_settings import *
